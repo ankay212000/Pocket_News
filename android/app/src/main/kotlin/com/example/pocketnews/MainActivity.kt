dc5e6ca776dc103c0f1e42b1349d2f73d02c5958
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-06-24 00:13:26 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/pocketnews/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pocketnews
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/check.dart
+++ lib/check.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:news/login.dart';
+import 'login.dart';
 import 'homepage.dart';
 
 
