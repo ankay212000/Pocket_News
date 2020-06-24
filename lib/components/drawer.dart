7f89a8858b79b3badb153f4963160114ce82d006
anand-aman <itsamananand@gmail.com>
2020-06-24 12:40:56 +0530

    Code Refactored. Extra Padding widget in Login Page removed.

---
+++ lib/components/drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pocketnews/screens/login_page.dart';
+
+class Draw extends StatefulWidget {
+  Draw({Key key, this.title, this.uid, this.email}) : super(key: key);
+  final String title;
+  final String uid;
+  final String email;
+  @override
+  _DrawState createState() => _DrawState();
+}
+
+class _DrawState extends State<Draw> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountEmail: Text(widget.email),
+            accountName: Text(widget.title),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.orangeAccent,
+              child: Text(
+                widget.title.toString()[0],
+                style: TextStyle(fontSize: 40.0),
+              ),
+              //Image.asset('assets/images/n.jpg'),
+            ),
+          ),
+          InkWell(
+            child: ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Log Out"),
+            ),
+            onTap: () {
+              FirebaseAuth.instance
+                  .signOut()
+                  .then(
+                    (result) => Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    ),
+                  )
+                  .catchError((err) => print(err));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/webview.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter/material.dart';
+
+class WebView extends StatelessWidget {
+  final String url, title;
+
+  WebView(this.url, this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: WebviewScaffold(
+        url: this.url,
+        appBar: AppBar(
+          title: Text(this.title),
+        ),
+      ),
+    );
+  }
+}
