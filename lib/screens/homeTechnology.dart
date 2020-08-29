bff3294291e63496a1a58b8bf92f2f39f0273e95
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-08-29 10:54:27 +0530

    Update homeTechnology.dart

--- lib/screens/homeTechnology.dart
+++ lib/screens/homeTechnology.dart
@@ -86,7 +86,7 @@ class _HomePageTState extends State<HomePageT> {
         backgroundColor: Colors.black,
         title: Text("Pocket News"),
         centerTitle: true,
-        actions: <Widget>[
+        /*actions: <Widget>[
           DropdownButtonHideUnderline(
             child: DropdownButton<String>(
               elevation: 15,
@@ -119,7 +119,7 @@ class _HomePageTState extends State<HomePageT> {
               }).toList(),
             ),
           ),
-        ],
+        ],*/
       ),
       body: RefreshIndicator(
         child: this.isLoaded
